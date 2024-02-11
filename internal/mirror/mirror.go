bcab915349d04b016d04018e8fcf39fb306ae021
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-02-11 17:39:56 -0600

    remove github.com/google/go-github/v58

--- internal/mirror/mirror.go
+++ internal/mirror/mirror.go
@@ -8,7 +8,7 @@ import (
 
 	"code.gitea.io/sdk/gitea"
 	configPkg "github.com/USA-RedDragon/gitea-mirror/internal/config"
-	"github.com/google/go-github/v58/github"
+	"github.com/google/go-github/v59/github"
 )
 
 type Mirror struct {
